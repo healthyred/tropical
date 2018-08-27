74e6b28591526bea30d51d7e391d8e2d1e381c9c
jian13579 <jianhlu018@gmail.com>
2018-08-27 13:48:30 -0400

    finished eigenvalue function

--- eigenvalueSimulation.py
+++ eigenvalueSimulation.py
@@ -6,16 +6,9 @@ import math
 import random
 
 MATRIX_SIZE = 2
+INF = float('inf')
 MAX_INT = 1 #(excluseive)
 MIN_INT = 0 #(inclusive)
-
-class Node(object):
-    """Node class"""
-    def __init__(self, data = None, children_weights = None):
-        self.data = data #we will label the data corresponding to the original index of the matrix
-        self.children_weights = next_node #this should be an array pointing to itself
-
-
 
 def genMatrix():
     return [[random.random() for y in range(0, MATRIX_SIZE)] for x in range(0, MATRIX_SIZE)]
@@ -34,7 +27,8 @@ def tropExp(A,n):
     return A_updated
 
 def tropMul(A, B):
-    result = [[MAX_INT * MAX_INT] * MATRIX_SIZE for i in range(MATRIX_SIZE)]
+    #result should be the size of the resulting matrix
+    result = [[INF] * len(B[0]) for i in range(len(B))]
     #a is a matrix, b is a matrix, assuming a and b are able to be multiplied".
     # iterate through rows of X
     for i in range(len(A)):
@@ -50,22 +44,75 @@ def tropMul(A, B):
 
 def findEigen(A):
     """Uses linear programming algorithm to return the eigenvalue,
-    λ(A), the minimumnormalized cycle length.
+    λ(A), the minimumnormalized cycle length. Implements using Karp's algorithm
     """
     ##todo create a graph class with nodes
-
     length = len(A)
-    candidate_values_set = set()
+    #arbitrary J in G(A)
+    j = 3
+    #construct an arbitrary identity element matrix of MATRIX_SIZE of A, and then arbitraily choose the jth column
+    identity_jth = identityVector(j,length) #this is the x(0)
 
-    return min(candidate_values_set)
+    ##initializes the set of candidates for the eigenvalues
+    candidate_values_set = []
+    ##Then loop through matrix an equivalent number of times to find the columns from x(0), x(1), ... x(n)
+    #print(identity_jth)
+
+    minimum_weight_matrix = [[] for i in range(length)] #a filler column
+    minimum_weight_matrix[0] = tropMul(A,identity_jth) #finds the x(1)
+
+    for i in range(length-1):
+        minimum_weight_matrix[i+1] = tropMul(A,minimum_weight_matrix[i])
+
+    minimum_weight_matrix.insert(0,identity_jth)
+    #print(minimum_weight_matrix)
+    eigen = maximize(minimum_weight_matrix)
+    #for i in range(len(to_min)):
+        #candidate_values_set.add(max(to_min[i]))
+    return eigen
+
+def identityVector(j,n):
+    """Takes in a number j, and the length of a matrix n, and returns of the jth column of an n x n
+    sized identiy matrix. Note that we start counting at 1."""
+    result = [[INF] for i in range(n)]
+    result[j-1] = [0]
+    return result
+
+def maximize(column_matrix):
+    """Takes in a minimum_weight_matrix and the first x_0, and returns a list of the possible values"""
+    maxed_weighted_lengths = []
+    length = len(column_matrix)
+    to_manipulate = column_matrix[length-1]
+
+    to_max = [[[] for i in range(length-1)] for j in range(length-1)]
+    for i in range(len(to_manipulate)):
+        for j in range(len(column_matrix[0])):
+            #print(to_manipulate[i][0])
+            #print(column_matrix[j][i][0])
+            #print(length-1-j)
+            to_max[i][j] = (to_manipulate[i][0] - column_matrix[j][i][0])/ (length-1 - j)
+            #print(to_max)
+    #print(max(to_max[0]))
+    for i in range(len(to_max)):
+        maxed_weighted_lengths.append(max(to_max[i]))
+    #print(min(maxed_weighted_lengths))
+    return min(maxed_weighted_lengths)
 
 if __name__ == "__main__":
-    A = genMatrix()
-    print(A)
-    B = genMatrix()
-    print(B)
-    C = tropExp(A,2)
-    D = tropExp(A,3)
-    print(C)
-    print(D)
-    print(len(C))
+
+    A = [[6, 7, INF, INF],[1, 4, INF, 6],[INF, 2, 4, 5],[INF, INF, 3, 6]]
+
+    testcase = identityVector(3,len(A))
+    #print(testcase)
+    x_1 = tropMul(A,testcase)
+    #print(x_1)
+    """
+    x_2 = tropMul(A,x_1)
+    print(x_2)
+    x_3 = tropMul(A,x_2)
+    x_4 = tropMul(A,x_3)
+    print(x_3)
+    print(x_4)
+    """
+    #print((20 - float('inf'))/4)
+    print(findEigen(A))
